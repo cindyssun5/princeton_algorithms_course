1fc0fb2ce294c7938ee5403da43c4412a610ca48
Cindy Sun <c.s.sun5@gmail.com>
2017-01-05 17:31:21 -0800

    wk2: complete randomizedqueue function with shuffle

--- wk2_deque_randomqueues.js
+++ wk2_deque_randomqueues.js
@@ -61,8 +61,8 @@ function Deque(item) {
           };
         } else if (!that.isEmpty() && (nextIndex + 1) >= end) {
           return {
-              done: true,
-            };
+            done: true,
+          };
         }
         return {
           value: "Can't call iterator on empty Deque",
@@ -73,3 +73,96 @@ function Deque(item) {
   };
 }
 
+
+function RandomizedQueue(item) {
+  const storage = {};
+  storage[0] = item;
+  let end = 1;
+  let size = 1;
+
+  this.isEmpty = function () {
+    return size === 0 ? true : false;
+  };
+
+  this.size = function () {
+    return size;
+  };
+
+  this.enqueue = function (val) {
+    storage[end] = val;
+    end += 1;
+    size += 1;
+  };
+
+  this.dequeue = function () {
+    if (this.isEmpty()) {
+      return 'Queue is empty, please add items before trying to dequeue';
+    }
+    const randomIndex = Math.floor(Math.random() * end);
+    const tempItem = storage[randomIndex];
+    size -= 1;
+
+    if (randomIndex === (end - 1)) {
+      delete storage[randomIndex];
+      end -= 1;
+      return tempItem;
+    } else {
+      end -= 1;
+      storage[randomIndex] = storage[end];
+      delete storage[end];
+      return tempItem;
+    }
+  };
+
+  this.sample = function () {
+    const randomIndex = Math.floor(Math.random() * end);
+    return storage[randomIndex];
+  };
+
+  this.iterator = function () {
+    let arr = [];
+    let nextIndex = -1;
+    for (let i = 0; i < size; i++) {
+      arr.push(i);
+    }
+    arr = shuffle(arr);
+    const that = this;
+
+    return {
+      next() {
+        nextIndex += 1;
+        if (that.isEmpty()) {
+          return {
+            value: "Can't call iterator on empty Queue",
+            done: "Can't call iterator on empty Queue",
+          };
+        }
+        if (nextIndex < size) {
+          return {
+            value: storage[arr[nextIndex]],
+            done: false,
+          };
+        }
+        if (nextIndex >= size) {
+          return {
+            done: true,
+          };
+        }
+      },
+    };
+  };
+}
+
+function shuffle(array) {
+  let counter = array.length;
+
+  while (counter > 0) {
+    const randomIndex = Math.floor(Math.random() * counter);
+    counter -= 1;
+    const temp = array[counter];
+    array[counter] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+
+  return array;
+}
